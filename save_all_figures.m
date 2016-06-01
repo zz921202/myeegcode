a9c7ebd828feac2699fb69f431c9673eacd24f94
Zhe <zz921202@gmail.com>
2016-06-01 10:37:35 -0500

    see graphs

--- save_all_figures.m
+++ save_all_figures.m
@@ -1,4 +1,4 @@
 h = get(0,'children');
 for i=1:length(h)
-  saveas(h(i), [pwd '/test_MIT_Data/' 'figure' num2str(i)], 'fig');
+  saveas(h(i), [pwd '/exam_graphs/' 'figure' num2str(i)], 'fig');
 end
